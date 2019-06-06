e56029cbbdda3a3d87a9954cf070f31a2b6a5067
maximilian_hanussek <maximilian.hanussek@uni-tuebingen.de>
2019-06-06 16:12:25 +0200

    Added volume deployment + attachment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,14 @@
 resource "openstack_blockstorage_volume_v2" "beeond_volume_master" {
-  name = "${var.name_prefix}beeond_volume_master"
-  size = "${var.beeond_disk_size}"
-  volume_type = "${var.beeond_storage_backend}"
+  name 		= "${var.name_prefix}beeond_volume_master"
+  size 		= "${var.beeond_disk_size}"
+  volume_type 	= "${var.beeond_storage_backend}"
+}
+
+resource "openstack_blockstorage_volume_v2" "beeond_volume_compute" {
+  count         = "${var.compute_node_count}"
+  name          = "${var.name_prefix}beeond_volume_compute-${count.index}"
+  size          = "${var.beeond_disk_size}"
+  volume_type   = "${var.beeond_storage_backend}"
 }
 
 
@@ -13,19 +20,19 @@ resource "openstack_compute_instance_v2" "master" {
   security_groups = "${var.security_groups}"
   network         = "${var.network}"
 
-#block_device {
-#    uuid                  = "${openstack_images_image_v2.vuc-image-master.id}"
-#    source_type           = "image"
-#    destination_type      = "local"
-#    boot_index            = 0
-#    delete_on_termination = true
-#  }
+block_device {
+    uuid                  = "${openstack_images_image_v2.vuc-image-master.id}"
+    source_type           = "image"
+    destination_type      = "local"
+    boot_index            = 0
+    delete_on_termination = true
+  }
 
 block_device {
     uuid                  = "${openstack_blockstorage_volume_v2.beeond_volume_master.id}"
-    source_type           = "blank"
+    source_type           = "volume"
     destination_type      = "volume"
-    boot_index            = 0
+    boot_index            = -1
     delete_on_termination = true
   }
 
@@ -39,4 +46,27 @@ resource "openstack_compute_instance_v2" "compute" {
   key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
   security_groups = "${var.security_groups}"
   network         = "${var.network}"
+
+block_device {
+    uuid                  = "${openstack_images_image_v2.vuc-image-compute.id}"
+    source_type           = "image"
+    destination_type      = "local"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+block_device {
+#    uuid                  = "${openstack_blockstorage_volume_v2.beeond_volume_compute.0.id}"
+     uuid		  = "${count.index != "0" ? "${openstack_blockstorage_volume_v2.beeond_volume_compute.1.id}" : "${openstack_blockstorage_volume_v2.beeond_volume_compute.0.id}"}"
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = -1
+    delete_on_termination = true
+  }
 }
+
+
+
+
+
+
