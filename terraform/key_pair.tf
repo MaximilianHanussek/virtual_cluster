83a444c30d5859705caf5e98378f5d2a248d5376
maximilian_hanussek <maximilian.hanussek@uni-tuebingen.de>
2019-06-24 15:55:17 +0200

    Deleting uncommented stuff

--- terraform/key_pair.tf
+++ terraform/key_pair.tf
@@ -1,15 +1,4 @@
-/*resource "openstack_compute_keypair_v2" "my-cloud-key" {
-  name       = "vuc-cloud-key"
-  public_key = "${var.public_key}"
-}*/
-
 resource "tls_private_key" "internal_connection_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-
-/*resource "local_file" "connection_key" {
-    content     = "${tls_private_key.internal_connection_key.private_key_pem}"
-    filename = "${path.module}/connection_key.pem"
-}*/
-
